c0d53586eb5216ecf9c3e355b62dd958fba66b34
Afiur Nur <afiurnur@gmail.com>
2025-03-10 00:28:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Hello, world!");
+
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+
+  let songs = []; // Initialize the array to store song URLs
+
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  return songs;
+}
+
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+
+  var audio = new Audio(songs[0]);
+  audio.play();
+}
+
+main();
